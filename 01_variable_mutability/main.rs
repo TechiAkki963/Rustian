8e07ba489cda66f6700b25482f36c3a1d2652742
Akshay Parab <paraba777@gmail.com>
2025-05-04 14:33:39 +0530

    Variable_mutability / updated Project

--- 01_variable_mutability/main.rs
+++ 01_variable_mutability/main.rs
@@ -1,3 +1,16 @@
+#[allow(unused_variable)]
 
+const TOUCH_DOWN: i32 = 6;
 
-let season: &str = 'Summer';
+fn main() {
+    let season: &str = "Summer";
+
+    let mut point_scored: i32 = 28;
+
+    point_scored = 35;
+
+    let event_time: &str = "6:00";
+    let event_time: i32 = 6;
+
+    let _favorite_bevrage = "coffee";
+}
